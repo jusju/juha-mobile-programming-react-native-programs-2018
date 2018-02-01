c5d01c61a2a64f551b8f7e694b7dfa2484c43a3d
jusju <jukka.juslin@haaga-helia.fi>
2018-02-01 11:25:48 +0200

    state for texts

--- react-native-number-guessing-game/App.js
+++ react-native-number-guessing-game/App.js
@@ -1,20 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert, Button, TextInput, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state= {text:''}
+    this.state= {text:'', rightNumber: '50', advice: 'Guess a number between 1-100' }
   }
+
+    guessThis = () => {
+      
+      if(parseInt(this.state.text) < parseInt(rightNumber)) {
+        this.setState(
+          {advice: 'Too low value!'}
+        );
+      } else if(parseInt(this.state.text) > parseInt(this.state.rightNumber)) {
+
+
+      } else {
+
+
+      }
+      Alert.alert(this.state.text);
+    };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Guess a number between 1-100</Text>
+        <Text>{this.state.advice}</Text>
 
-        <TextInput style={{width:200, borderColor: 'gray', borderWidth:1}} 
+        <TextInput keyboardType='numeric' style={{width:200, borderColor: 'gray', borderWidth:1}} 
           onChangeText={(text) => this.setState({text})}
           value={this.state.text}/>
+
+        <Button onPress={this.guessThis} title="MAKE A GUESS" />
 
       </View>
     );
